6c22ad41098bf983201760a0cace6427dbfddc2b
Daniel <josedanielalves018@gmail.com>
2024-03-25 21:01:18 -0300

    dialogos criados

--- addWireframe.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-from addWireframeDialog import Ui_AddWireframeDialog
-
-class AddWireframe(object):  
-    def __init__(self):
-      super().__init__()
-      self.ui = Ui_AddWireframeDialog()
-      self.ui.setupUi(self)
